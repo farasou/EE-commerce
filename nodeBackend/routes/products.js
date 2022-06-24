3ecfacd56d188a1f04a960439a716765e4caf21f
AymKh <aymen4real@gmail.com>
2022-06-24 21:49:39 +0100

    products crud almost there

--- nodeBackend/routes/products.js
+++ nodeBackend/routes/products.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 const Product = require('../models/Product');
 
+// Middleware to make life easier
+const getProduct = async (req, res, next)=>{
+    let foundProduct;
+    try {
+        foundProduct = await Product.findById(req.params.id);
+        if (!foundProduct)
+        return res.status(404).json({'ID Not Found': req.params.id});
+    } catch (error) {
+        res.json({'error while retrieving product ::': error.message});
+    }
+    res.foundProduct = foundProduct;
+    next();
+}
 
+
+/*
+    REST API for products TO PERFORM BASIC CRUD OPERATIONS
+*/
+
+
+// ## GET ALL
+router.get('/', async (req, res)=> {
+    try {
+        const productsList = await Product.find();
+        res.json(productsList);
+    } catch (error) {
+        res.status(500).json({ 'Error while retrieving the products ::': error.messaage});
+    }
+})
 
 
 
