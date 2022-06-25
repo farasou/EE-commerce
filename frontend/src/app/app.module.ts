23577e78e56dc6e43ae1afd3165f41e1d9b4372a
AymKh <aymen4real@gmail.com>
2022-06-25 03:32:30 +0100

    products grid initial teo=mplate

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './pages/header/header.component';
 import { HeroFilterComponent } from './pages/hero-filter/hero-filter.component';
+import { ProductsGridComponent } from './pages/products-grid/products-grid.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroFilterComponent
+    HeroFilterComponent,
+    ProductsGridComponent
   ],
   imports: [
     BrowserModule,
