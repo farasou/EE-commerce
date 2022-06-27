af1cfc3642ad3d4741116e8f2311f8e7e733d437
AymKh <aymen4real@gmail.com>
2022-06-27 17:36:44 +0100

    routing and minor styling refactor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductManageComponent} from "./pages/product-manage/product-manage.component";
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'manage-product', component: ProductManageComponent },
+  { path: '**', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ProductManageComponent, HomeComponent];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './pages/header/header.component';
 import { HeroFilterComponent } from './pages/hero-filter/hero-filter.component';
 import { ProductsGridComponent } from './pages/products-grid/products-grid.component';
 import { ProductCardComponent } from './pages/products-grid/product-card/product-card.component';
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroFilterComponent,
     ProductsGridComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    routingComponents,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
